a970850327349e04ee3d33f548e9bfd398e93b56
prajjwal98dubey <prajjwaldubey003@gmail.com>
2025-08-27 21:28:40 +0530

    uploading a file from client to server with multer

--- text/index.js
+++ text/index.js
@@ -2,6 +2,30 @@ const fullBtn = document.getElementById("full-btn");
 const chunkBtn = document.getElementById("chunk-btn");
 const container = document.getElementById("container");
 const totalLength = document.getElementById("length");
+const inp = document.getElementById("file-inp");
+
+inp.addEventListener("change", async (e) => {
+  const file = e.target.files[0];
+  const fileSize = file.size;
+  const chunk = 1000;
+  const totalChunks = Math.ceil(fileSize / chunk);
+
+  for (let i = 0; i < totalChunks; i++) {
+    let start = i * chunk;
+    let end = start + chunk;
+
+    let chunkFile = file.slice(start, end + 1);
+    const formData = new FormData();
+    formData.append("sample", chunkFile);
+    formData.append("index", i);
+    formData.append("fileName", file.name);
+    await fetch("http://localhost:8081/upload_file", {
+      method: "POST",
+      body: formData,
+    });
+  }
+});
+
 fullBtn.addEventListener("click", async () => {
   let res = await fetch("http://localhost:8081/full_data");
   res = await res.text();
--- text/server.js
+++ text/server.js
@@ -1,13 +1,25 @@
 import express from "express";
 import cors from "cors";
 import fs from "fs";
+import multer from "multer";
 const app = express();
+const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(
   cors({
     origin: "*",
   })
 );
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/upload_file", upload.single("sample"), (req, res) => {
+  const { fileName, index } = req.body;
+  const filePath = process.cwd() + `/uploads/${fileName}`;
+  fs.appendFileSync(filePath, req.file.buffer);
+  console.log(`${index} chunk running`);
+
+  res.send("file uploading !!!");
+});
 
 app.get("/chunk_data", async (req, res) => {
   const filePath = process.cwd() + "/demo.txt";
